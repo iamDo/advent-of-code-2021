4c8ead8731e83ab49d391adcb9b0c6f2ac33ec32
ppolmans <ppolmans@inuits.eu>
2021-12-01 14:02:06 +0100

    puzzle-1: add second solution

---
+++ puzzle-1/soln2.py
@@ -0,0 +1,6 @@
+measurements = []
+with open('./input', 'r') as input_file:
+    measurements = [int(line.strip()) for line in input_file.readlines()]
+
+increments = sum([1 if measurements[index + 1] > measurements[index] else 0 for index in range(0, len(measurements) - 1)])
+print(increments)
