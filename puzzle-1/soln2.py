edf98469f8141c3f276a28180b4a9f6aecc2b2a9
iamDo <trianoware@gmail.com>
2021-12-01 14:34:40 +0100

    puzzle-1: soln2: make more readable

--- puzzle-1/soln2.py
+++ puzzle-1/soln2.py
@@ -2,5 +2,7 @@ measurements = []
 with open('./input', 'r') as input_file:
     measurements = [int(line.strip()) for line in input_file.readlines()]
 
-increments = sum([1 if measurements[index + 1] > measurements[index] else 0 for index in range(0, len(measurements) - 1)])
+increments = sum([1 if measurements[index + 1] > measurements[index] else 0
+    for index in range(0, len(measurements) - 1)])
+
 print(increments)
