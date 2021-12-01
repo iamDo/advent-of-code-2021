ca8fd7ec1448048f066f895baee3f0892c3390d8
iamDo <trianoware@gmail.com>
2021-12-01 16:38:46 +0100

    puzzle-1: soln1: remove useless no_change and decrement counters

--- puzzle-1/soln1.py
+++ puzzle-1/soln1.py
@@ -3,8 +3,6 @@ with open('./input', 'r') as input_file:
     read_lines = input_file.readlines()
 
 increments = 0
-decrements = 0
-no_change = 0
 prev_line = int(read_lines[0].strip())
 cur_line = int(read_lines[0].strip())
 for line in read_lines:
@@ -12,11 +10,5 @@ for line in read_lines:
     cur_line = int(line.strip())
     if cur_line > prev_line:
         increments = increments + 1
-    elif prev_line > cur_line:
-        decrements = decrements + 1
-    else:
-        no_change = no_change + 1
 
 print(increments)
-print(decrements)
-print(no_change)
