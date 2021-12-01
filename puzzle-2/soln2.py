3bcd722c7cf8270e4abef7761a989987a4135883
iamDo <trianoware@gmail.com>
2021-12-01 14:29:04 +0100

    puzzle-2: soln2: remove window_width and measurements from get_sum_from_index function and make final list comprehension more readable

--- puzzle-2/soln2.py
+++ puzzle-2/soln2.py
@@ -4,9 +4,10 @@ window_width = 3
 with open('./input', 'r') as input_file:
     measurements = [int(line.strip()) for line in input_file.readlines()]
 
-def get_sum_from_index(index, window_width, measurements):
+def get_sum_from_index(index):
     return sum([measurements[i] for i in range(index, index + window_width)])
 
-increments = sum([1 if get_sum_from_index(index + 1, window_width, measurements) > get_sum_from_index(index, window_width, measurements) else 0 for index in range(0, (len(measurements) - window_width))])
+increments = sum([1 if get_sum_from_index(index + 1) > get_sum_from_index(index) else 0
+    for index in range(0, (len(measurements) - window_width))])
 
 print(increments)
